0844ca9469f043699414130dc536fdfd0a5a37d3
Francesco <frank_hdz16@hotmail.com>
2022-02-15 00:00:00 +1510

    modify reusable component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  ariaLabel: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div
+    class="app-avatar"
+    :aria-label="ariaLabel"
+    role="img"
+  >
+    <span>
+      {{ name[0].toUpperCase() }}
+    </span>
+  </div>
+</template>
+
+<style lang="scss">
+  .app-avatar {
+    border-radius: 100px;
+    width: 1.5rem;
+    height: 1.5rem;
+    background: v-bind(color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    user-select: none;
+    span {
+      color: var(--white-two);
+      transform: translateY(1px);
+    }
+  }
+</style>
